416d3b812bf56f3df450b92eb423553dd9501fdb
mufrick01 <mrmufrick@gmail.com>
2024-11-25 00:58:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# admin-shop
+
+1. Clonar repositorio
+2. instalar dependencias
+3. crear archivo .env basado en .env.template
+4. correr proyecto : `npm run dev`
---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <TopMenu />
+
+
+
+  <RouterView />
+
+  <!-- Footer -->
+  <FooterMenu />
+</template>
+
+<script setup lang="ts">
+import FooterMenu from '../components/FooterMenu.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
